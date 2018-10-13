d711b031f2f0950cb33c4749f132bd10b1f9c8fd
shrpne <shrpne@gmail.com>
2018-10-13 16:51:30 +0300

    fix track

--- js/include/track.js
+++ js/include/track.js
@@ -1,9 +1,13 @@
-const search = window.location.search;
+let search = window.location.search;
 if (search) {
+    // if no query referrer
+    if (search.indexOf('&referrer=') === -1 && search.indexOf('?referrer=') === -1) {
+        const referrer = document.referrer;
+        search += referrer ? '&referrer=' + referrer : '';
+    }
+
     const frame = document.createElement('iframe');
-    const referrer = document.referrer;
-    const query = referrer ? search + '&referrer=' + referrer : search;
-    frame.src = 'https://minternetwork.com/api/v1/statistic/track' + query;
+    frame.src = 'https://minternetwork.com/api/v1/statistic/track' + search;
     frame.style.position = 'absolute';
     frame.style.left = '-9999px';
     frame.style.width = '1px';
