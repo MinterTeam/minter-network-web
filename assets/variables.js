04df99a571e94c3b0b506862c5f4ff9b8067dc8c
shrpne <shrpne@gmail.com>
2020-03-10 13:30:25 +0300

    MN-224: show tx and block count with history data

--- assets/variables.js
+++ assets/variables.js
@@ -4,4 +4,6 @@ export const BASE_DESCRIPTION = 'Create a cryptocurrency of your own. All inform
 export const LANGUAGE_COOKIE_KEY = 'minter-language';
 
 export const STATUS_API_URL = process.env.APP_STATUS_API_URL;
+export const HISTORY_BLOCK_COUNT = 5000000;
+export const HISTORY_TRANSACTION_COUNT = 5405605;
 
--- layouts/_footer.vue
+++ layouts/_footer.vue
@@ -1,6 +1,7 @@
 <script>
     import {getStatus} from "~/api/status.js";
     import {prettyRound} from "~/assets/utils";
+    import {HISTORY_BLOCK_COUNT, HISTORY_TRANSACTION_COUNT} from '~/assets/variables.js';
 
     export default {
         prettyRound,
@@ -8,6 +9,14 @@
             return {
                 stats: null,
             };
+        },
+        computed: {
+            numberOfBlocks() {
+                return this.stats ? this.stats.numberOfBlocks + HISTORY_BLOCK_COUNT : '';
+            },
+            txTotalCount() {
+                return this.stats ? this.stats.txTotalCount + HISTORY_TRANSACTION_COUNT : '';
+            },
         },
         beforeMount() {
             getStatus()
@@ -34,11 +43,11 @@
             </div>
             <div class="footer__stats-item">
                 <div class="footer__stats-key">Blocks</div>
-                <div class="footer__stats-value">{{ $options.prettyRound(stats.numberOfBlocks) }}</div>
+                <div class="footer__stats-value">{{ $options.prettyRound(numberOfBlocks) }}</div>
             </div>
             <div class="footer__stats-item">
                 <div class="footer__stats-key">Transactions</div>
-                <div class="footer__stats-value">{{ $options.prettyRound(stats.txTotalCount) }}</div>
+                <div class="footer__stats-value">{{ $options.prettyRound(txTotalCount) }}</div>
             </div>
             <div class="footer__stats-item">
                 <div class="footer__stats-key">BIPs mined</div>
