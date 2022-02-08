865efe3bc58fed96c954ba0e9be1f40ecaef6f11
2z2z2z <61245073+2z2z2z@users.noreply.github.com>
2022-02-08 11:20:48 +0300

    torronet landing page

---
+++ pages/ru/torronet.vue
@@ -0,0 +1,163 @@
+<script>
+import {getPool, getStatus} from '~/api/explorer.js';
+import {prettyRound, pretty, getApy} from '~/assets/utils.js';
+import {HOST} from '~/assets/variables.js';
+import trackClick from '~/assets/v-track-click.js';
+import Language from '~/layouts/_language.vue';
+import Footer from '~/layouts/_footer.vue';
+
+export default {
+    layout: 'simple',
+    components: {
+        Language,
+        Footer,
+    },
+    directives: {
+        trackClick,
+    },
+    fetchOnServer: false,
+    head() {
+        const title = 'Minter 3';
+        const description = 'Большое обновление Токеномики. Внедрение лучших практик блокчейн-индустрии.';
+        // const localeSuffix = this.$i18n.locale === 'en' ? '' : '-' + this.$i18n.locale;
+
+        return {
+            title: title + ' | Minter Network',
+            meta: [
+                { hid: 'og-title', name: 'og:title', content: title },
+                { hid: 'description', name: 'description', content: description },
+                { hid: 'og-description', name: 'og:description', content: description },
+                { hid: 'og-image', name: 'og:image', content: `/torronet/images/meta-ru.png` },
+            ],
+            link: [
+                { rel: 'canonical', href: `${HOST}/torronet`},
+                { rel: 'stylesheet', href: '/torronet/style.css?2', hid: 'torronet-style'},
+                { rel: 'stylesheet', href: '/torronet/style320.css?2', media: '(max-width: 1020px)', hid: 'torronet-style320'},
+            ],
+        };
+    },
+    mounted() {
+        // move landing styles under global styles
+        const style = document.querySelector('[data-hid="torronet-style"]');
+        if (!style) {
+            return;
+        }
+        style.parentNode.removeChild(style);
+        document.head.appendChild(style);
+    },
+};
+</script>
+
+<template>
+    <div class="wrap">
+        <div class="u-container u-container--full index-language lang-wrap">
+            <language class="" :locales='[{"code":"","name":"English"},{"code":"ru","name":"Russian"}]' lang="ru" :push="true"/>
+        </div>
+        <header>
+            <div class="container">
+                <div class="hello">
+                    <div class="hello-content">
+                        <div class="logo"><a href="#"><img src="/img/minter-logo-circle.svg" width="60" height="60" alt="Minter 3"/></a></div>
+                        <div class="torronet">Minter 3</div>
+                        <h1>Большое <br />Обновление Токеномики</h1>
+                        <p>Внедрение лучших практик блокчейн-индустрии.</p>
+                        <a class="btn" href="https://www.minter.network/how-to-buy-and-sell-bip" target="_blank" v-track-click="'buy-bipx'">Купить и застэйковать BIP</a>
+                    </div>
+                    <img class="hello-image" src="/torronet/images/hero-image.png" srcset="/torronet/images/hero-image@2x.png 2x" />
+                </div>
+            </div>
+        </header>
+
+        <section class="stats">
+            <div class="container">
+                <div class="stats-content">
+                    <div class="stats-item">
+                        <div class="stats-value">$15,132,113</div>
+                        <h4>Рыночная капитализация</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">$27,425,575</div>
+                        <h4>Капитализация при полной эмиссии</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">15 мая 2019 г.</div>
+                        <h4>Запуск основной сети</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">10,000,000,000</div>
+                        <h4>Максимальное предложение</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">5,423,536,352 (54%)</div>
+                        <h4>Циркулирующее предложение</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">1,532,771,084 (15%)</div>
+                        <h4>В свободном обращении</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">46</div>
+                        <h4>Активных валидаторов</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">12,747</div>
+                        <h4>Активных стэйкеров</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="drivers">
+            <div class="container">
+                <h2>Драйверы роста BIP</h2>
+                <div class="drivers-content">
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d1.svg" alt="Динамический майнинг">
+                        <h3>Динамический майнинг</h3>
+                        <p>Цена BIP падает, когда предложение монет на рынке превышает спрос. Чтобы снизить влияние этого фактора, мы вводим балансировщик вознаграждений за блок, который уменьшает блок-реварды при падении цены BIP, помогая избежать дальнейшего избытка BIP. В то же время когда цена BIP растет, балансировщик увеличивает награды, чтобы максимизировать результат стэйкинга.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d2.svg" alt="Автоределегирование">
+                        <h3>Автоределегирование</h3>
+                        <p>В данный момент все награды за делегирование начисляются на адреса делегаторов примерно каждый час. Автоределегирование изменит эту механику таким образом, что награды будут автоматически делегироваться, что приведет к увеличению как стэйка, так и самих наград. Для получения ревардов пользователю будет необходимо сделать стандартную транзакцию анбонда в нужном количестве монет.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d3.svg" alt="Сжигание балансировщиком">
+                        <h3>Сжигание балансировщиком</h3>
+                        <p>Если цена BIP падает, это не замедляет выпуск новых монет, то есть не растягивает срок конечной эмиссии. В этом случае нераспределенные награды (до базового значения) будут сжигаться через отправку на нулевой адрес.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d4.svg" alt="Сжигание комиссий за тикеры">
+                        <h3>Сжигание комиссий за тикеры</h3>
+                        <p>Создавая новый токен, пользователь оплачивает комиссию за тикер, которая зависит от его длины — чем короче тикер, тем больше комиссия. Вместо того, чтобы распределяться валидаторам и их делегаторам, эта комиссия будет сжигаться через отправку на нулевой адрес, уменьшая количество BIP в свободном обращении.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d5.svg" alt="Выкуп BIP">
+                        <h3>Выкуп BIP</h3>
+                        <p>При обмене внутри пула ликвидности BIP выкупается и сжигается. Мы повысим комиссию пула с текущих 0.2% до среднерыночных 0.3%. Провайдеры ликвидности будут по-прежнему зарабатывать 0.2% от обменов в пуле, а остальные 0.1% будут использоваться для откупа BIP и его дальнейшего сжигания.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d6.svg" alt="Трехлетняя блокировка">
+                        <h3>Трехлетняя блокировка</h3>
+                        <p>С выходом апргейда Minter 3 пользователям будет предложен новый вариант стэйкования. Они смогут отправить свои BIP в делегирование с периодом заморозки в 3 года и получать в 3 раза больше вознаграждений. В то же время комиссии, собираемые валидаторами, не увеличатся. Для применения этой функции пользователям не нужно будет делать анбонд.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d7.svg" alt="Перенос стэйка">
+                        <h3>Перенос стэйка</h3>
+                        <p>Мы представим транзакцию MoveStake, которая переносит стэйк новому валидатору. Процесс переноса займет 7 дней.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="text">
+            <div class="container">
+                <p><b>Все вышеперечисленные меры преследуют одну цель — стимулировать дефицит BIP.</b><br />
+                Даже если спрос <i>остается прежним</i>, а предложение <i>сокращается</i>, цены, как правило, <i>растут</i>.</p>
+                <a class="btn" href="https://www.minter.network/how-to-buy-and-sell-bip" target="_blank" v-track-click="'buy-bipx'">Купить и застэйковать BIP</a>
+            </div>
+        </section>
+
+        <Footer class="footer--transparent"/>
+    </div>
+</template>
---
+++ pages/torronet/index.vue
@@ -0,0 +1,163 @@
+<script>
+import {getPool, getStatus} from '~/api/explorer.js';
+import {prettyRound, pretty, getApy} from '~/assets/utils.js';
+import {HOST} from '~/assets/variables.js';
+import trackClick from '~/assets/v-track-click.js';
+import Language from '~/layouts/_language.vue';
+import Footer from '~/layouts/_footer.vue';
+
+export default {
+    layout: 'simple',
+    components: {
+        Language,
+        Footer,
+    },
+    directives: {
+        trackClick,
+    },
+    fetchOnServer: false,
+    head() {
+        const title = 'Minter 3';
+        const description = 'Grand tokenomics update. Implementation of the best blockchain industry’s practices.';
+        // const localeSuffix = this.$i18n.locale === 'en' ? '' : '-' + this.$i18n.locale;
+
+        return {
+            title: title + ' | Minter Network',
+            meta: [
+                { hid: 'og-title', name: 'og:title', content: title },
+                { hid: 'description', name: 'description', content: description },
+                { hid: 'og-description', name: 'og:description', content: description },
+                { hid: 'og-image', name: 'og:image', content: `/torronet/images/meta.jpg` },
+            ],
+            link: [
+                { rel: 'canonical', href: `${HOST}/torronet`},
+                { rel: 'stylesheet', href: '/torronet/style.css?2', hid: 'torronet-style'},
+                { rel: 'stylesheet', href: '/torronet/style320.css?2', media: '(max-width: 1020px)', hid: 'torronet-style320'},
+            ],
+        };
+    },
+    mounted() {
+        // move landing styles under global styles
+        const style = document.querySelector('[data-hid="torronet-style"]');
+        if (!style) {
+            return;
+        }
+        style.parentNode.removeChild(style);
+        document.head.appendChild(style);
+    },
+};
+</script>
+
+<template>
+    <div class="wrap">
+        <div class="u-container u-container--full index-language lang-wrap">
+            <language class="" :locales='[{"code":"","name":"English"},{"code":"ru","name":"Russian"}]' lang="" :push="true"/>
+        </div>
+        <header>
+            <div class="container">
+                <div class="hello">
+                    <div class="hello-content">
+                        <div class="logo"><a href="#"><img src="/img/minter-logo-circle.svg" width="60" height="60" alt="Minter 3"/></a></div>
+                        <div class="torronet">Minter 3</div>
+                        <h1>Grand <br />Tokenomics Update</h1>
+                        <p>Implementation of the best blockchain industry’s practices.</p>
+                        <a class="btn" href="https://www.minter.network/how-to-buy-and-sell-bip" target="_blank" v-track-click="'buy-bipx'">Buy and Stake BIP</a>
+                    </div>
+                    <img class="hello-image" src="/torronet/images/hero-image.png" srcset="/torronet/images/hero-image@2x.png 2x" />
+                </div>
+            </div>
+        </header>
+
+        <section class="stats">
+            <div class="container">
+                <div class="stats-content">
+                    <div class="stats-item">
+                        <div class="stats-value">$15,132,113</div>
+                        <h4>Market Cap</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">$27,425,575</div>
+                        <h4>Fully Diluted Market Cap</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">May 15, 2019</div>
+                        <h4>Mainnet Launch</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">10,000,000,000</div>
+                        <h4>Max. Supply</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">5,423,536,352 (54%)</div>
+                        <h4>Circulating Supply</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">1,532,771,084 (15%)</div>
+                        <h4>Free Float</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">46</div>
+                        <h4>Active Validators</h4>
+                    </div>
+                    <div class="stats-item">
+                        <div class="stats-value">12,747</div>
+                        <h4>Active Stakers</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="drivers">
+            <div class="container">
+                <h2>BIP Growth Drivers</h2>
+                <div class="drivers-content">
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d1.svg" alt="Dynamic Mining">
+                        <h3>Dynamic Mining</h3>
+                        <p>BIP’s price drops when the supply of coins on the market exceeds the demand. To make this less impactful, we introduce a block reward balancer that reacts by reducing block rewards when the BIP price falls, helping to >avoid further BIP surplus. At the same time, when the BIP price goes up, the balancer increases the rewards to maximize staking outcome.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d2.svg" alt="Auto-Redelegation">
+                        <h3>Auto-Redelegation</h3>
+                        <p>Currently, all delegation rewards are sent to addresses of delegators about once every hour. Auto-redelegation will change things in a way that all rewards will be automatically delegated, increasing the stake as well as> the rewards. To claim rewards, the user will need to make a regular unbond transaction, specifying the number of coins they need.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d3.svg" alt="Burning by Balancer">
+                        <h3>Burning by Balancer</h3>
+                        <p>If the price of BIP goes down, it doesn’t slow down the mining of new coins, meaning the deadline for reaching total supply doesn’t change. In this case, undistributed rewards (down to the base value) will be burned via being sent to the 0th address.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d4.svg" alt="Burning of Ticker Fees">
+                        <h3>Burning of Ticker Fees</h3>
+                        <p>When creating a new coin or token, the issuer pays the fee that depends on the length of the ticker—the shorter, the cheaper. Instead of being distributed to validators and their delegators, this fee will now be sent to the 0th address and burned, taking the coins out of free circulation.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d5.svg" alt="BIP Buyback">
+                        <h3>BIP Buyback</h3>
+                        <p>When swaps are made within liquidity pools, BIP is bought back and later burned. We’ll increase the in-pool fee from .2% to .3% (market average). Liquidity providers will still be earning .2% on swaps within pools, while the remaining .1% will go towards BIP buyback and burn.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d6.svg" alt="3-Year BIP Lock">
+                        <h3>3-Year BIP Lock</h3>
+                        <p>A new staking option will be offered to users with Minter 3 Upgrade. They will be able to send their BIP into delegation with a 3-year lock and receive 3x rewards. At the same time, fees collected by validators will not increase. To apply this feature users won't need to unbond their stakes.</p>
+                    </div>
+                    <div class="drivers-item">
+                        <img src="/torronet/images/d7.svg" alt="Stake Transfer">
+                        <h3>Stake Transfer</h3>
+                        <p>We will introduce a MoveStake transaction that transfers the stake to a new validator. The transfer will take 7 days.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="text">
+            <div class="container">
+                <p><b>All measures above share a single goal, which is to stimulate BIP shortage.</b><br />
+                Even if demand <i>remains the same</i> while there is a <i>decrease</i> in supply, prices tend to <i>rise</i>.</p>
+                <a class="btn" href="https://www.minter.network/how-to-buy-and-sell-bip" target="_blank" v-track-click="'buy-bipx'">Buy and Stake BIP</a>
+            </div>
+        </section>
+
+        <Footer class="footer--transparent"/>
+    </div>
+</template>
