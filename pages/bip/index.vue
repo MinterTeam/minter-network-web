17df449ac94600459e7c9136979f14cb21b6a361
2z2z2z <61245073+2z2z2z@users.noreply.github.com>
2023-03-27 15:59:06 +0300

    BIP: text upd

--- pages/bip/index.vue
+++ pages/bip/index.vue
@@ -742,7 +742,7 @@ export default {
                     <div class="roadmap-content">
                         <h2>Roadmap</h2>
                         <p>In light of the changes that leading blockchains have recently made in their tokenomics, Minter Team is proposing a large-scale update of the network. Its goal is to improve the economic model and implement deflationary and counter-inflationary mechanisms, stimulating the strengthening of BIP.</p>
-                        <p>The Minter team has started to design mechanisms outlined below. We plan to gradually roll them out in Q1 and Q2 2022.</p>
+                        <p>The Minter team has started to design mechanisms outlined below. We plan to gradually roll them out in 2023.</p>
                         <a href="https://minterteam.medium.com/minter-3-bip-tokenomics-update-c6e81264e33c" target="_blank" rel="nofollow" class="btn btn-alt">Read More</a>
                     </div>
                     <div class="roadmap-image">
